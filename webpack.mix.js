155f8e13e918da15398a368aff5676d624e20e96
oiuv <i@oiuv.cn>
2018-07-30 21:53:23 +0800

    npm run prod

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/bootstrap.js', 'public/js')
+mix.js('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css')
     .copyDirectory('resources/assets/editor/js', 'public/js')
     .copyDirectory('resources/assets/editor/css', 'public/css');
