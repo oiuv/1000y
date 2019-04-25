9675db83b63ac14c223ce03585b138f55d23ccf4
oiuv <i@oiuv.cn>
2019-04-25 21:48:09 +0800

    update laravel to v5.8

--- routes/web.php
+++ routes/web.php
@@ -12,42 +12,42 @@
 */
 
 
-$this->get('/', 'PagesController@root')->name('root');
+Route::get('/', 'PagesController@root')->name('root');
 
 // Auth::routes();
 // Authentication Routes...
-$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
-$this->post('login', 'Auth\LoginController@login');
-$this->post('logout', 'Auth\LoginController@logout')->name('logout');
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
 // Registration Routes...
-$this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
-$this->post('register', 'Auth\RegisterController@register');
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
 
 // Password Reset Routes...
-$this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-$this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-$this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-$this->post('password/reset', 'Auth\ResetPasswordController@reset');
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 // Route::get('/home', 'HomeController@index')->name('home');
 
-$this->resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
-$this->resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
-$this->get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
-$this->resource('categories', 'CategoriesController', ['only' => ['show']]);
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
-$this->post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
-$this->resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
+Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
 
-$this->resource('notifications', 'NotificationsController', ['only' => ['index']]);
+Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
 
-$this->get('permission-denied', 'PagesController@permissionDenied')->name('permission-denied');
+Route::get('permission-denied', 'PagesController@permissionDenied')->name('permission-denied');
 
 // 针对客户端的固定链接
-$this->get('download.html', 'PagesController@download')->name('download');
-$this->get('account.html', 'PagesController@account')->name('account');
+Route::get('download.html', 'PagesController@download')->name('download');
+Route::get('account.html', 'PagesController@account')->name('account');
 
 // 游戏数据库
-$this->get('sdb/{name}', 'SdbController@index')->name('sdb.index');
-$this->get('sdb/{name}/{id}', 'SdbController@show')->name('sdb.show');
+Route::get('sdb/{name}', 'SdbController@index')->name('sdb.index');
+Route::get('sdb/{name}/{id}', 'SdbController@show')->name('sdb.show');
