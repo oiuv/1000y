e4ce14bf1b17baa9cbd5d79719e6ce531030db05
Xuefeng <i@oiuv.cn>
2022-03-03 20:11:43 +0800

    调整注册成功转跳下载页

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -29,7 +29,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectTo = '/download.html'; // 转到下载
 
     /**
      * Create a new controller instance.
