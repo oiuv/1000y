a448749e46b81576e8f1a0e681ddfa63c60d2cd9
Xuefeng <i@oiuv.cn>
2022-03-03 12:54:52 +0800

    调整redis缓存驱动，更新设置

--- config/database.php
+++ config/database.php
@@ -120,7 +120,7 @@ return [
 
     'redis' => [
 
-        'client' => 'predis',
+        'client' => env('REDIS_CLIENT', 'phpredis'),
 
         'default' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
